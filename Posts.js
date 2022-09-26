9dc819b1fc1e69ed9de871ec411e16b6bf954f4d
beno1234 <benolopesdias@gmail.com>
2022-09-26 03:02:13 -0300

    Portal de noticias Beno Lopes Dias

--- Posts.js
+++ Posts.js
@@ -7,7 +7,9 @@ var postSchema = new Schema({
     imagem:String,
     categoria:String,
     conteudo:String,
-    slug:String
+    slug:String,
+    autor:String,
+    views: Number
 }, {collection: 'posts'})
 
 
